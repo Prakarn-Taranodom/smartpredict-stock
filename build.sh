20c35518080677331af741c8f75516d006bad626
Prakarn Taranodom <prakarntaranodom@gmail.com>
2026-01-20 15:42:54 +0700

    Fix Python version compatibility

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-apt-get update
-apt-get install -y build-essential gcc g++ gfortran libopenblas-dev liblapack-dev
-
-pip install --upgrade pip setuptools wheel
-pip install Cython numpy
+pip install --upgrade pip
 pip install -r requirements.txt
